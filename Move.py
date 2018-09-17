6b97c3227eda5a68f54e51d7a6785ce194c5293f
pupukung2007 <piyanat.ku@ku.th>
2018-09-17 23:10:45 +0700

    Update Move.py
    Add SpecialMove subclass to this file instead

--- Move.py
+++ Move.py
@@ -11,6 +11,23 @@ class Move(ABC):
         pass
 
 
+class SpecialMove(Move):
+    def __init__(self,name,power):
+        super().__init__(name,power)
+
+
+    def use(self,user_pokemon,rival_pokemon):
+        damage = ( (((2*user_pokemon.level/5)+2) *self.power*(user_pokemon.special_attack / rival_pokemon.special_defense) /50)+2  )
+        rival_pokemon.set_hp(rival_pokemon.hp - damage)
+        return str(user_pokemon.name+" uses "+self.name+" on "+rival_pokemon.name+" for "+str(damage)+" damage.")
+
+
+
+
+
+
+
+
 
 
 
